8b05a1c25f1dc22473e3b01600c6d185a1cab4d9
Tomasz Gabrysiak <tomsonwch@gmail.com>
2017-02-02 14:49:37 +0100

    Remove commented code

--- webpack.config.js
+++ webpack.config.js
@@ -15,33 +15,26 @@ module.exports = {
     resolve: {extensions: ['.js', '.json']},
     stats: {colors: true, reasons: true, chunks: false},
     module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.js$/,
-                loader: 'eslint-loader',
-                exclude: /node_modules/
-            }, {
-                include: path.resolve(__dirname, 'src'),
-                test: /\.js$/,
-                loader: 'babel-loader'
-            }, {
-                test: /\.css$/,
-                use: ['style-loader', {loader: 'css-loader', options: {url: false}}]
-            }],
+        rules: [{
+            enforce: 'pre',
+            test: /\.js$/,
+            loader: 'eslint-loader',
+            exclude: /node_modules/
+        }, {
+            include: path.resolve(__dirname, 'src'),
+            test: /\.js$/,
+            loader: 'babel-loader'
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', {loader: 'css-loader', options: {url: false}}]
+        }],
         loaders: [
             {
                 loader: "babel-loader",
-
-                // Skip any files outside of your project's `src` directory
                 include: [
                     path.resolve(__dirname, "src")
                 ],
-
-                // Only run `.js` and `.jsx` files through Babel
                 test: /\.js$/,
-
-                // Options to configure babel with
                 query: {
                     plugins: ['transform-runtime'],
                     presets: ['es2015']
@@ -50,25 +43,3 @@ module.exports = {
         ]
     }
 };
-/*
-
-module.exports = {
-    context: __dirname,
-    entry: './src/Demo1/ClientApp.js',
-    devtool: 'source-map',
-    output: {path: path.join(__dirname, '/public'), filename: 'bundle.js'},
-    devServer: {publichPath: '/public/'},
-    resolve: {extensions: ['.js', '.json']},
-    stats: {colors: true, reasons: true, chunks: false},
-    module: {
-        rules: [{
-            enforce: 'pre',
-            test: /\.js$/,
-            loader: 'eslint-loader',
-            exclude: /node_modules/
-        }, {include: path.resolve(__dirname, 'src'), test: /\.js$/, loader: 'babel-loader'}, {
-            test: /\.css$/,
-            use: ['style-loader', {loader: 'css-loader', options: {url: false}}]
-        }]
-    }
-} */
