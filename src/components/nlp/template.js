346c013cf9e7bc1d39dfcce7eb3698543501cc8f
Patryk Barczak <patryk.barczak@infermedica.com>
2020-06-04 13:52:11 +0200

    Correct typo in a note

--- src/components/nlp/template.js
+++ src/components/nlp/template.js
@@ -18,7 +18,7 @@ const template = (context) => {
         <p>Identified observations:</p>
         <ul class="list-unstyled" id="observations">
         </ul>
-        <p class="text-muted small"><i class="fa fa-exclamation-circle"></i> Please not that you can go <span class="badge badge-primary">Next</span> only if there are <span class="text-success">present <i class="fa fa-plus-circle"></i></span> identified observations.</p>
+        <p class="text-muted small"><i class="fa fa-exclamation-circle"></i> Please note that you can go <span class="badge badge-primary">Next</span> only if there are <span class="text-success">present <i class="fa fa-plus-circle"></i></span> identified observations.</p>
         <p class="text-muted small"><i class="fa fa-info-circle"></i> This screen uses our NLP engine to find symptoms in a written text. Evidence found in text will be marked as initial which is important to our engine. Please read more about initial evidence <a target="_blank" href="https://developer.infermedica.com/docs/diagnosis#gathering-initial-evidence">here</a>. All of the identified symptoms will be added to your interview after clicking <span class="badge badge-primary">Next</span>.</p>
       </div>
     `);
